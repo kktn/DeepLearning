d80b6492655b885095e8860cc24f0b623d477d30
kktn <miracle.kktn@gmail.com>
2017-04-10 00:12:20 -0700

    Implemented OR

--- chap2/perceptron.py
+++ chap2/perceptron.py
@@ -24,7 +24,14 @@ def NAND(x1, x2):
         return 0
 
 def OR(x1, x2):
-    pass
+    x = numpy.array([x1, x2])
+    w = numpy.array([0.5, 0.5])
+    b = -0.4
+    val = numpy.sum(x*w) + b
+    if val > 0:
+        return 1
+    else:
+        return 0
 
 raw_args = input()
 args = raw_args.split()
