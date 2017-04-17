8062e8c10d8436d76758e39eb73a932c33c687ed
kktn <miracle.kktn@gmail.com>
2017-04-17 15:31:39 +0900

    show sigmoid function chart

--- chap3/activation_function.py
+++ chap3/activation_function.py
@@ -14,8 +14,8 @@ def sigmoid(x):
 # plt.ylim(-0.1, 1.1)
 # plt.show()
 
-x = np.array([-1.0, 1.0, 2.0])
+x = np.arange(-5.0, 5.0, 0.1)
 y = sigmoid(x)
 plt.plot(x, y)
-plt.ylim(-0.1, 1.0)
+plt.ylim(-0.1, 1.1)
 plt.show()
