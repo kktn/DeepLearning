cae5d386e4aed41225ba85bb3033c50ac845995b
kktn <miracle.kktn@gmail.com>
2017-04-17 17:16:22 +0900

    implemented some transimissions

--- chap3/activation_function.py
+++ chap3/activation_function.py
@@ -10,7 +10,11 @@ def sigmoid(x):
 def relu(x):
     return np.maximum(0, x)
 
-def neuron1():
+def identity_function(x):
+    return x
+
+def neuron():
+    # initial layer -> first layer
     X = np.array([1.0, 0.5])
     W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
     B1 = np.array([0.1, 0.2, 0.3])
@@ -24,6 +28,24 @@ def neuron1():
 
     Z1 = sigmoid(A1)
     print("Z1 is {0}".format(Z1))
+
+    # first layer -> second layer
+    W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    B2 = np.array([0.1, 0.2])
+
+    A2 = np.dot(Z1, W2) + B2
+    Z2 = sigmoid(A2)
+    print("Z2 is {0}".format(Z2))
+
+    # second layer -> output layer
+    W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+    B3 = np.array([0.1, 0.2])
+
+    A3 = np.dot(Z2, W3) + B3
+    Y = identity_function(A3)
+
+    print("Result: {0}".format(Y))
+
 
 # Step function
 # x = np.arange(-5.0, 5.0, 0.1)
@@ -42,4 +64,4 @@ def neuron1():
 # ReLU function
 # print(relu(1))
 
-neuron1()
+neuron()
