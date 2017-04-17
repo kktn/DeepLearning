b4c29f0b1056edb7f25a7f8e8a91155be2c8949a
kktn <miracle.kktn@gmail.com>
2017-04-17 15:53:25 +0900

    Added relu function

--- chap3/activation_function.py
+++ chap3/activation_function.py
@@ -7,6 +7,9 @@ def step_function(x):
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def relu(x):
+    return np.maximum(0, x)
+
 # Step function
 # x = np.arange(-5.0, 5.0, 0.1)
 # y = step_function(x)
@@ -14,8 +17,11 @@ def sigmoid(x):
 # plt.ylim(-0.1, 1.1)
 # plt.show()
 
-x = np.arange(-5.0, 5.0, 0.1)
-y = sigmoid(x)
-plt.plot(x, y)
-plt.ylim(-0.1, 1.1)
-plt.show()
+# Sigmoid function
+# x = np.arange(-5.0, 5.0, 0.1)
+# y = sigmoid(x)
+# plt.plot(x, y)
+# plt.ylim(-0.1, 1.1)
+# plt.show()
+
+print(relu(1))
